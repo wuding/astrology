6e815b900359ba312ed08c6da7108c2a2d4ca6c5
wuding <netjoin@gmail.com>
2019-05-18 18:05:09 +0800

    修复视图路径：相对路径前缀绝对目录

--- app/Play/Controller/_Controller.php
+++ app/Play/Controller/_Controller.php
@@ -1,47 +1,50 @@
 <?php
+/**
+ * 按名称播放条目
+ */
 namespace Controller;
 
 use Astrology\Route;
 
 class _Controller extends \Astrology\Controller
 {
-	public $tongji = null;
-	public $playlist = [];
-	
-	public function __construct()
-	{
-		$stat = 0;
-		if (isset($_GET['stat'])) {
-			$stat = $_GET['stat'];
-			setcookie('stat', $stat, time()+60*60*24*30, '/');
-		} elseif (isset($_COOKIE['stat'])) {
-			$stat = $_COOKIE['stat'];
-		}
-		$this->tongji = $stat;
-	}
-	
-	public function __call($name, $arguments)
-	{
-		$tongji = $this->tongji;		
-		$m3u8 = new \DbTable\HlsM3u8;		
-		$title = '在线M3U8播放器';
-		$url = '';
-		$hide = 0;
-		$where = null;
-		$like = '';
+    public $tongji = null;
+    public $playlist = [];
 
-		$row = $m3u8->findByName($name);
-		if ($row) {
-			$url = $row->url;
-			$like = $title = $row->title;
-			$hide = 1;
-			if ($row->playlist) {
-				$where = ['playlist' => $row->playlist];
-			}
-		}
-		$arr = $m3u8->fetchAll($where, 'title,name');
+    public function __construct()
+    {
+        $stat = 0;
+        if (isset($_GET['stat'])) {
+            $stat = $_GET['stat'];
+            setcookie('stat', $stat, time()+60*60*24*30, '/');
+        } elseif (isset($_COOKIE['stat'])) {
+            $stat = $_COOKIE['stat'];
+        }
+        $this->tongji = $stat;
+    }
 
-		include '../app/_Module/View/Index/play.php';
-		exit;
-	}
+    public function __call($name, $arguments)
+    {
+        $tongji = $this->tongji;
+        $m3u8 = new \DbTable\HlsM3u8;
+        $title = '在线M3U8播放器';
+        $url = '';
+        $hide = 0;
+        $where = null;
+        $like = '';
+
+        $row = $m3u8->findByName($name);
+        if ($row) {
+            $url = $row->url;
+            $like = $title = $row->title;
+            $hide = 1;
+            if ($row->playlist) {
+                $where = ['playlist' => $row->playlist];
+            }
+        }
+        $arr = $m3u8->fetchAll($where, 'title,name');
+
+        include __DIR__ . '/../../../app/_Module/View/Index/play.php';
+        exit;
+    }
 }
